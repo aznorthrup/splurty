f9e41c99cc6d07678f5d7e609880d1ee7ed577ed
Bryce Northrup <aznorthrup@gmail.com>
2021-07-06 20:21:25 +0000

    Updated gemfile

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,10 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 //= require jquery3
+//= require jquery
+//= require jquery_ujs
 //= require popper
+//= require tether
 //= require bootstrap-sprockets 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
