69ba188f3707ed3889d95c47f6f422bf5d038af8
Bryce Northrup <aznorthrup@gmail.com>
2021-07-07 17:56:26 +0000

    various changes to HTML

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,13 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-//= require jquery3
-//= require jquery
-//= require jquery_ujs
-//= require popper
-//= require tether
-//= require bootstrap-sprockets 
-//= require turbolinks
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
