d62fade963be5dac0b84754afe10daa90a14a17c
Bryce Northrup <aznorthrup@gmail.com>
2021-07-08 00:10:32 +0000

    Completed Contribution page.

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -7,4 +7,18 @@ def new
     @quote = Quote.new
 end
 
+def create
+    @quote = Quote.create(quote_params)
+    if @quote.invalid?
+      flash[:error] = '<strong>Hold on!</strong> That wasn\'t quite long enough. Please try again.'
+    end
+    redirect_to root_path
+end
+
+private
+
+def quote_params
+    params.require(:quote).permit(:saying, :author)
+end
+
 end
